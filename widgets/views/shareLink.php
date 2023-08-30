4635fe161cdaea2927725e0c365d53392f09c79c
Erik Schnittker <erik.schnittker@lcsi.co>
2023-08-30 16:22:16 +0200

    Updated share links

--- widgets/views/shareLink.php
+++ widgets/views/shareLink.php
@@ -26,9 +26,10 @@ Assets::register($this);
 
 			";
 		?>
-        	<?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode((string)$permalink) . '&description=' . urlencode((string)$object->getContentDescription()), ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink) . '&ro=false', ['onclick' => $option]); ?>
-        	<?= Html::a('<i class="fa fa-share" style="font-size:16px;color:#00c300">&nbsp;</i>', 'https://social-plugins.line.me/lineit/share?' . '&text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+		<?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode((string)$permalink) . '&description=' . urlencode((string)$object->getContentDescription()), ['onclick' => $option]); ?>
+        <?= Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink), ['onclick' => $option]); ?>
+        <?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode((string)$object->getContentDescription()) . '&url=' . urlencode((string)$permalink) . '&ro=false', ['onclick' => $option]); ?>
+		<?= Html::a('<i class="fa fa-xing-square" style="font-size:16px;">&nbsp;</i>', 'https://www.xing.com/spi/shares/new?url=' . urlencode((string)$permalink), ['onclick' => $option]) ?>
+		<?= Html::a('<i class="fa fa-envelope-o" style="font-size:16px;color:#0177b5">&nbsp;</i>', 'mailto:?subject=LCSI-Intranet%20-%20'.$object->getContentDescription().'&body=Schau%20mal%2C%20was%20es%20hier%20Neues%20gibt...%20'.urlencode($permalink) );?>
 	</div>
 </span>
